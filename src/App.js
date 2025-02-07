a5c18b2e542e9779539ece0dc2970f3e3796f040
Uniunt🍥⚓ <106765520+Uniunt@users.noreply.github.com>
2025-02-07 17:00:12 +0800

    使用 Link 组件替代 <a> 标签

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import News from './News';
 import Movies from './Movies';
 import { Layout, Menu } from 'antd';
@@ -9,26 +9,26 @@ const { Header, Content } = Layout;
 
 function App() {
   return (
-    <Layout className="layout">
-      <Header>
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-          <Menu.Item key="1">
-            <a href="/news">百度新闻</a>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <a href="/movies">豆瓣电影</a>
-          </Menu.Item>
-        </Menu>
-      </Header>
-      <Content style={{ padding: '0 50px', marginTop: '16px' }}>
-        <Router>
+    <Router>
+      <Layout className="layout">
+        <Header>
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1">
+              <Link to="/news">百度新闻</Link>
+            </Menu.Item>
+            <Menu.Item key="2">
+              <Link to="/movies">豆瓣电影</Link>
+            </Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px', marginTop: '16px' }}>
           <Routes>
             <Route path="/news" element={<News />} />
             <Route path="/movies" element={<Movies />} />
           </Routes>
-        </Router>
-      </Content>
-    </Layout>
+        </Content>
+      </Layout>
+    </Router>
   );
 }
 
