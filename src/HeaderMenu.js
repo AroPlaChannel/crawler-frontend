9723cb31546bd2b66111e4f47f3edd2316add298
Uniunt🍥⚓ <106765520+Uniunt@users.noreply.github.com>
2025-03-06 20:34:46 +1100

    修改tab导航问题

--- src/HeaderMenu.js
+++ src/HeaderMenu.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { signOut } from 'firebase/auth';
 import { auth } from './firebaseConfig';
 import { Menu } from 'antd';
@@ -15,19 +16,19 @@ function HeaderMenu() {
   return (
     <Menu theme="dark" mode="horizontal">
       <Menu.Item key="1">
-        <a href="/news">百度新闻</a>
+        <Link to="/news">百度新闻</Link>
       </Menu.Item>
       <Menu.Item key="2">
-        <a href="/movies">豆瓣电影</a>
+        <Link to="/movies">豆瓣电影</Link>
       </Menu.Item>
       <Menu.Item key="3">
-        <a href="/recognize_digit">手写数字识别</a>
+        <Link to="/recognize_digit">手写数字识别</Link>
       </Menu.Item>
       <Menu.Item key="logout" onClick={handleLogout}>
         退出
       </Menu.Item>
     </Menu>
-  );
+  );  
 }
 
 export default HeaderMenu;
